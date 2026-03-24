f92c4fb0d341b0248d0e4bbdb9f014e2f0af6c92
PonyNatu <papasanfukumaru@gmail.com>
2026-03-24 21:16:33 +0900

    Update BattleStartPatch.java

--- src/ruinacompat/patches/BattleStartPatch.java
+++ src/ruinacompat/patches/BattleStartPatch.java
@@ -7,7 +7,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import java.util.ArrayList;
 
-@SpirePatch2(clz = AbstractRoom.class, method = "initializeMonstersForBattle")
+@SpirePatch2(clz = AbstractRoom.class, method = "onPlayerEntry")
 public class BattleStartPatch {
 
     @SpirePostfixPatch
@@ -18,9 +18,11 @@ public class BattleStartPatch {
             ArrayList<AbstractMonster> copy = new ArrayList<>(__instance.monsters.monsters);
             for (AbstractMonster m : copy) {
                 if (m == null) continue;
+
                 if (m.maxHealth <= 0) {
                     m.maxHealth = 1;
                 }
+
                 if (m.currentHealth <= 0 && !m.isDead && !m.isDying) {
                     System.out.println("[RuinaCompatMod] BattleStartPatch: fixing HP for " + m.name);
                     m.currentHealth = 1;
