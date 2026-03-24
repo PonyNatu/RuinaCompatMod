ecdd1779db6a34bb586a8bfedb4b944d8a74e666
PonyNatu <papasanfukumaru@gmail.com>
2026-03-24 22:15:13 +0900

    Update DamageAllEnemiesActionPatch.java

--- src/ruinacompat/patches/DamageAllEnemiesActionPatch.java
+++ src/ruinacompat/patches/DamageAllEnemiesActionPatch.java
@@ -4,6 +4,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 @SpirePatch2(clz = DamageAllEnemiesAction.class, method = "update")
@@ -12,18 +13,13 @@ public class DamageAllEnemiesActionPatch {
     @SpirePrefixPatch
     public static SpireReturn<Void> prefix(DamageAllEnemiesAction __instance) {
         try {
-            if (AbstractDungeon.getCurrRoom() == null) {
-                __instance.isDone = true;
-                return SpireReturn.Return(null);
-            }
-            if (AbstractDungeon.getCurrRoom().monsters == null) {
-                __instance.isDone = true;
-                return SpireReturn.Return(null);
+            for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+                if (m != null && (m.isDead || m.isDying)) {
+                    // 必要ならログ
+                }
             }
         } catch (Exception e) {
             System.out.println("[RuinaCompatMod] DamageAllEnemiesActionPatch error: " + e.getMessage());
-            __instance.isDone = true;
-            return SpireReturn.Return(null);
         }
         return SpireReturn.Continue();
     }
